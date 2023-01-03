0a94bffd5a5892754d7f89c417b2a2e143ef6be8
Leandro Cavalcante <lleandroo93@gmail.com>
2023-01-02 21:32:50 -0400

    add responsive_builder dependency

--- lib/views/login/login_view.dart
+++ lib/views/login/login_view.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:romaniz/views/home/dashboard/dashboard_view.dart';
-import 'package:romaniz/views/home/home_view.dart';
 
 class LoginView extends StatelessWidget {
   static const String route = '/login';
@@ -14,63 +12,66 @@ class LoginView extends StatelessWidget {
         alignment: Alignment.center,
         child: ConstrainedBox(
           constraints: BoxConstraints.loose(const Size(400, 400)),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'ROMANIZ',
-                style: TextStyle(
-                  color: Colors.blue,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 24,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'ROMANIZ',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                  ),
                 ),
-              ),
-              Form(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        label: Text('Login'),
-                        prefixIcon: Icon(Icons.email),
-                      ),
-                    ),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        label: Text('Senha'),
-                        prefixIcon: Icon(Icons.lock_rounded),
-                      ),
-                    ),
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: TextButton(
-                        onPressed: () {},
-                        child: const Text('Esqueci minha senha'),
-                      ),
-                    ),
-                    const SizedBox(height: 32),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: TextButton(
-                            onPressed: () {},
-                            child: const Text('Criar conta'),
-                          ),
+                Form(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          label: Text('Login'),
+                          prefixIcon: Icon(Icons.email),
                         ),
-                        Expanded(
-                          child: ElevatedButton(
-                            onPressed: () => GoRouter.of(context).go("/home"),
-                            //Navigator.of(context).pushNamed(HomeView.routePrefixHome + DashboardView.route),
-                            child: const Text('Entrar'),
-                          ),
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          label: Text('Senha'),
+                          prefixIcon: Icon(Icons.lock_rounded),
                         ),
-                      ],
-                    )
-                  ],
+                      ),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: TextButton(
+                          onPressed: () {},
+                          child: const Text('Esqueci minha senha'),
+                        ),
+                      ),
+                      const SizedBox(height: 32),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: TextButton(
+                              onPressed: () {},
+                              child: const Text('Criar conta'),
+                            ),
+                          ),
+                          Expanded(
+                            child: ElevatedButton(
+                              onPressed: () => GoRouter.of(context).go("/home"),
+                              //Navigator.of(context).pushNamed(HomeView.routePrefixHome + DashboardView.route),
+                              child: const Text('Entrar'),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
